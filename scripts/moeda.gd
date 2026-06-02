30c5fd169aa6f9035aba93ad3819b67d7043f654
Marcelo Manzo <marcelohmanzo@gmail.com>
2026-06-02 10:57:23 -0300

    Adição de Inimigos, Expansão do mapa e Animações estruturadas
    Adição de Inimigos (com colisão). Expansão do mapa. Animações estruturadas (das moedas e dos inimigos). Adição de um Backgroud

--- scripts/moeda.gd
+++ scripts/moeda.gd
@@ -1,11 +1,6 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("eu sou uma moeda")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 moeda")
+	queue_free()
