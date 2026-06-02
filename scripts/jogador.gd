4ef7abaf68d7692183de3fe4e998089bd437a43a
Marcelo Manzo <marcelohmanzo@gmail.com>
2026-06-02 11:23:56 -0300

    Adição de um contador de Moedas
    Criação de um script em um nó Único (game Manager) que adiciona moedas ao passar por elas.

--- scripts/jogador.gd
+++ scripts/jogador.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# Handle jump. (Mudado de "ui_accept" para "pular" ou "ui_accept" com a tecla W configurada)
 	if Input.is_action_just_pressed("pular") and is_on_floor():
@@ -14,7 +17,25 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# Mudado de "ui_left" e "ui_right" para suas ações customizadas
+	
+	#obtem a direção de entrada: -1,0,1
 	var direction := Input.get_axis("esquerda", "direita")
+	#mudar direção
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	#Animações
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+		
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
