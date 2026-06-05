e9ec71c065b70212c6eef9bb2df2bf49ff50a02e
Haruo09 <fabio.hn01@gmail.com>
2026-06-05 20:00:58 -0300

    chore: atualizações para novos padrões e melhor legibilidade

--- scripts/jogador.gd
+++ scripts/jogador.gd
@@ -2,14 +2,13 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity += get_gravity() * delta
 
 	# Handle jump. (Mudado de "ui_accept" para "pular" ou "ui_accept" com a tecla W configurada)
 	if Input.is_action_just_pressed("pular") and is_on_floor():
@@ -18,22 +17,21 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# Mudado de "ui_left" e "ui_right" para suas ações customizadas
 	
-	#obtem a direção de entrada: -1,0,1
+	# obtem a direção de entrada: -1,0,1
 	var direction := Input.get_axis("esquerda", "direita")
-	#mudar direção
+	# mudar direção
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 	
-	#Animações
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
-	else:
+	# Animações
+	if not is_on_floor():
 		animated_sprite.play("jump")
+	elif direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
 		
 	
 	if direction:
