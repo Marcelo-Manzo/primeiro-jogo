35fa436886a50281a6b337085ffbf635d15f44e0
Haruo09 <fabio.hn01@gmail.com>
2026-06-05 19:39:11 -0300

    chore: moedas posicionadas via TileMapLayer para posicionamento ótimo no Grid.

--- scripts/moeda.gd
+++ scripts/moeda.gd
@@ -1,8 +1,8 @@
 extends Area2D
-@onready var game_manager: Node = %GameManager
 
+@onready var game_manager: Node = $"../../../GameManager"
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	game_manager.add_point()
 	queue_free()
